106bead9eeb25c7291aeb154e17095f8e2786c76
Durotan <janfojtik@icloud.com>
2023-10-29 19:31:35 +0100

    added layouts and frame.

--- src/JavaSwing.java
+++ src/JavaSwing.java
@@ -2,20 +2,29 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.EventListener;
+import java.awt.Image;
+import java.awt.FlowLayout;
+import java.awt.Color;
 
 public class JavaSwing implements ActionListener {
 
     public static void main(String[] args) {
-
-
-        JPanel panel = new JPanel(); //to je layout.. musime jako prvni vlozit okno na layout
-        panel.setLayout(null);
-
-
+        JPanel panel = new JPanel(new FlowLayout());
         JFrame frame = new JFrame();
+
+
+        ImageIcon icon = new ImageIcon("C:\\Users\\fojtik.AC\\Downloads\\1147.jpg");
+        Image image = icon.getImage();
+
+
+
+        //JPanel panel = new JPanel(); //to je layout.. musime jako prvni vlozit okno na layout
+        panel.setLayout(null);
         frame.setSize(350, 350); //frame je okno
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.add(panel);
+        frame.setIconImage(image);
+        frame.getContentPane().setBackground(new Color(192, 192, 192)); // Light gray
 
 
         //user text
@@ -24,7 +33,6 @@ public class JavaSwing implements ActionListener {
         panel.add(userLabel);
 
         //user kolonka
-
         JTextField userText = new JTextField();
         userText.setBounds(100, 40, 165, 25);
         panel.add(userText);
@@ -49,38 +57,12 @@ public class JavaSwing implements ActionListener {
 
         frame.setVisible(true);
     }
-}
 
-        // button.setBounds();
-
-        //password kolonka
-
-        //user window where u will put input
-
-    /*@Override
-    public void actionPerformed(ActionEvent e) {
-
-        System.out.println("Button Clicked");
-
-    }
-}
     @Override
     public void actionPerformed(ActionEvent e) {
-        String username = userText.getText();
-        String password = new String(pass.getPassword());
 
-        // Přidejte kód pro uložení username a password do MongoDB kolekce
-        Document document = new Document("username", username)
-                .append("password", password);
-        collection.insertOne(document);
-
-        System.out.println("Data saved to MongoDB");
-        import com.mongodb.MongoClient;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
-import org.bson.Document;
+        System.out.println("Clicked homie!");
 
     }
+}
 
-
-     */
